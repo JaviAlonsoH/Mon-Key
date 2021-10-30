a4d9c005869df4ae1f1b305a91d77b6567066485
Javi Alonso <javier.alonso@live.u-tad.com>
2021-10-30 16:42:39 +0200

    Import Login/SignUp Templates

---
+++ dbh.php
@@ -0,0 +1,8 @@
+<?php
+$databasename = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "tienda_Mon-Key";
+
+$conn = mysqli_connect($databasename, $dbUsername, $dbPassword, $dbName);
+?>
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+    include_once 'dbh.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon-Key</title>
+</head>
+<body>
+
+<?php
+    $sql = "SELECT * FROM USER;";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+
+    if ($resultCheck > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo $row['name'];
+        }
+    }
+?>
+
+<ul>
+    <?php
+    $sql = "SELECT * FROM PRODUCTS;";
+    $result = mysqli_query($conn, $sql);
+        $resultCheck = mysqli_num_rows($result);
+
+    if ($resultCheck > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<li> $row['name'] . $row['price']</li>";
+    }
+}
+?>
+
+</ul>
+
+</body>
+</html>
