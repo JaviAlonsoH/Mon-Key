00a0d5c0d1439520d4ac3f33b7191382beb934c4
Javi Alonso <javier.alonso@live.u-tad.com>
2021-12-08 16:06:21 +0100

    Carta de producto terminada

--- component.php
+++ component.php
@@ -2,36 +2,34 @@
 
 function component($productName, $productPrice, $productImg)
 {
-    $element = "
-    <div class=\"col-md-3 col-sm-6 my-md-0\">
-                <form action=\"index.php\" method=\"post\">
-                    <div class=\"card shadow\">
+    $element = null ?>
+    <div class="col-md-3 col-sm-6 my-md-0">
+                <form action="index.php" method="post">
+                    <div class="card shadow">
                         <div>
-                            <img src=\" <?php echo 'data:image/jpeg;base64,' . base64_encode($productImg); ?>\" alt=\"Logo\" class=\"img-fluid card-img-top\"/>
+                            <img src="<?php echo 'data:image/jpeg; base64,' . base64_encode($productImg) ?>" alt="Logo" class="card-img-top">
                         </div>
-                        <div class=\"card-body\">
-                            <h5 class=\"card-title\">$productName</h5>
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $productName ?></h5>
                             <h6>
-                                <i class=\"fas fa-star\"></i>
-                                <i class=\"fas fa-star\"></i>
-                                <i class=\"fas fa-star\"></i>
-                                <i class=\"fas fa-star\"></i>
-                                <i class=\"far fa-star\"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="far fa-star"></i>
                             </h6>
-                            <p class=\"card-text\">
-                                Some quick example text to build on the card.
-                            </p>
                             <h5>
-                                <small><s class=\"text-secondary\">$productPrice</s></small>
-                                <span class=\"price\">$35</span>
+                                <small><s class="text-secondary"><?php echo '$' . ($productPrice + 15) ?></s></small>
+                                <span class="price"><?php echo '$' . $productPrice ?></span>
                             </h5>
-                            <button type=\"submit\" class=\"btn btn-warning my-3\" name=\"add\">Add to Cart <i class=\"fas fa-shopping-cart\"></i></button>
+                            <button type="submit" class="btn btn-warning my-3" name="add">Add to Cart <i class="fas fa-shopping-cart"></i></button>
                         </div>
                     </div>
                 </form>
             </div>
 
-    ";
-
+<?php 
     echo $element;
 }
+
+?>
